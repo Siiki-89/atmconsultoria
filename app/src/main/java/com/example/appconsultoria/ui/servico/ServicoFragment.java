f5bb3e55d8602cdff013ae7c204545b732403432
Siiki-89 <alexandreee.melo@hotmail.com>
2026-04-30 14:30:24 -0300

    first commit

---
+++ app/src/main/java/com/example/appconsultoria/ui/servico/ServicoFragment.java
@@ -0,0 +1,33 @@
+package com.example.appconsultoria.ui.servico;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.appconsultoria.R;
+
+public class ServicoFragment extends Fragment {
+
+
+    public ServicoFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_servico, container, false);
+    }
+}
